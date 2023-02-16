d5616a5a5ec77ea2c3250c8d868a967b9d15f8d3
Nikita Karetkin <nikita-kareta@yandex.ru>
2023-02-16 21:42:09 +0300

    exercsises had been accomplished

---
+++ Homework7/exercise_47/Program.cs
@@ -0,0 +1,28 @@
+﻿double[,] Array = new double[3, 4];
+double[,] RandomFillArray(double[,] Array)
+{
+    for (int i = 0; i < Array.GetLength(0); i++)
+    {
+        for (int j = 0; j < Array.GetLength(1); j++)
+        {
+            Array[i, j] = new Random().NextDouble() * 10;
+        }
+    }
+    return Array;
+}
+
+void PrintArray(double[,] Array)
+{
+    for (int i = 0; i < Array.GetLength(0); i++)
+    {
+        for (int j = 0; j < Array.GetLength(1); j++)
+        {
+            Console.Write($"{Math.Round(Array[i, j],2)} ");
+        }
+        Console.WriteLine();
+    }
+
+}
+
+RandomFillArray(Array);
+PrintArray(Array);
---
+++ Homework7/exercise_50/Program.cs
@@ -0,0 +1,46 @@
+﻿int[,] MultiDimensionalArray()
+{
+    int[,] Array = new int[3, 4];
+    return Array;
+}
+
+int[,] RandomFillArray(int[,] Array)
+{
+    for (int i = 0; i < Array.GetLength(0); i++)
+    {
+        for (int j = 0; j < Array.GetLength(1); j++)
+        {
+            Array[i, j] = new Random().Next(-10, 11);
+        }
+    }
+    return Array;
+}
+
+void PrintArray(int[,] Array)
+{
+    for (int i = 0; i < Array.GetLength(0); i++)
+    {
+        for (int j = 0; j < Array.GetLength(1); j++)
+        {
+            Console.Write($"{Array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+
+}
+
+void DigitPrint(int[,] Array)
+{
+    Console.WriteLine("Enter a number of the row: ");
+    int i = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Enter a number of the column: ");
+    int j = Convert.ToInt32(Console.ReadLine());
+
+    if (i <= Array.GetLength(0) && j <= Array.GetLength(1)) Console.WriteLine($"The number of the positions is: {Array[i,j]}");
+    else Console.WriteLine("The number with specified positions doesn't exist :(");
+}
+
+int[,] Array = RandomFillArray(MultiDimensionalArray());
+PrintArray(Array);
+Console.WriteLine();
+DigitPrint(Array);
