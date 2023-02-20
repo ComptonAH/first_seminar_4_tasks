b1d86bc6ff422c6f3d203c4e053b9ca0e700cd5d
Nikita Karetkin <nikita-kareta@yandex.ru>
2023-02-20 23:07:17 +0300

    exercise 55 had been acomplished

---
+++ Seminar 7/exercise_55/Program.cs
@@ -0,0 +1,43 @@
+﻿int[,] RandomFillArray(int rows, int columns)
+{
+    int[,] array = new int[rows, columns];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(-10, 11);
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+    return array;
+}
+
+
+int[,] transposeArray(int[,] Array, int columns,int rows)
+{
+    Console.WriteLine();
+    int[,] transposedArray = new int[columns, rows];
+    for (int i = 0; i < Array.GetLength(1); i++)
+    {
+        for (int j = 0; j < Array.GetLength(0); j++)
+        {
+            transposedArray[i, j] = Array[j, i];
+            Console.Write($"{transposedArray[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+    return transposedArray;
+}
+
+int rows = Convert.ToInt32(Console.ReadLine());
+if (rows < 0) 
+{
+    Console.WriteLine("You have to enter a positive number. Try again");
+}
+else
+{
+    int columns = Convert.ToInt32(Console.ReadLine());
+    if (columns < 0) Console.WriteLine("You have to enter a positive number. Try again");
+    else transposeArray(RandomFillArray(rows,columns),columns,rows);
+}
